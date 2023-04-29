944f4fe43b1f94b779dd4425b9b67aafee4b3901
Semih <semihgullboy@hotmail.com>
2023-04-30 00:33:52 +0300

    KareDefterOyunu

---
+++ Kare Defter/Kare Defter/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Kare_Defter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puan : 0";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 46);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(460, 449);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(482, 516);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kare Defter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Kare Defter/Kare Defter/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kare_Defter
+{
+    public partial class Form1 : Form
+    {
+        int Alan, puan = 0;
+        bool hamle = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonolusturma();
+        }
+
+         private void buttonolusturma()
+        {
+            puan = 0;
+            label1.Text = "Puan : 0";
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnCount = 9;
+            Alan = 81;
+            for (int i = 0; i < Alan; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(50, 50);
+                button.Tag = 0;
+                button.Click += Button_Click;
+                tableLayoutPanel1.Controls.Add(button);
+                button.BackColor = Color.White;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button tiklanan = (Button)sender;
+            puan++;
+            label1.Text = "Puan : " + puan;
+            tiklanan.Text = puan.ToString();
+            Sifirla();
+            for (int i = 0; i < Alan; i++)
+            {
+                tableLayoutPanel1.Controls[i].Enabled = false;
+            }
+            tiklanan.Enabled = false;
+            tiklanan.Tag = 1;
+            Hamleler(tiklanan);
+            if (!hamle)
+            {
+                if (puan == Alan)
+                {
+                    MessageBox.Show("Kazandınız  \n Puanınız : 81" );
+                }
+                else
+                {
+                    MessageBox.Show(" Kaybettiniz  \n Puanınız :" + puan);
+                }
+            }
+        }
+
+        public void Sifirla()
+        {
+            for (int i = 0; i < Alan; i++)
+            {
+                tableLayoutPanel1.Controls[i].BackColor = Color.White;
+            }
+            hamle = false;
+        }
+
+        public void Hamleler(Button btn)
+        {
+            int i;
+            for (i = 0; i < Alan; i++)
+            {
+                if (puan.ToString() == tableLayoutPanel1.Controls[i].Text)
+                    break;
+            }
+            if (0 <= (i - (2 * tableLayoutPanel1.ColumnCount) + 1) && (tableLayoutPanel1.ColumnCount - 1) - (i % tableLayoutPanel1.ColumnCount) >= 1 && tableLayoutPanel1.Controls[(i - (2 * tableLayoutPanel1.ColumnCount) + 1)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i - (2 * tableLayoutPanel1.ColumnCount) + 1].Enabled = true;
+                tableLayoutPanel1.Controls[i - (2 * tableLayoutPanel1.ColumnCount) + 1].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if (0 <= (i - (2 * tableLayoutPanel1.ColumnCount) - 1) && (i % tableLayoutPanel1.ColumnCount) >= 1 && tableLayoutPanel1.Controls[(i - (2 * tableLayoutPanel1.ColumnCount) - 1)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i - (2 * tableLayoutPanel1.ColumnCount) - 1].Enabled = true;
+                tableLayoutPanel1.Controls[i - (2 * tableLayoutPanel1.ColumnCount) - 1].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if ((i + (2 * tableLayoutPanel1.ColumnCount) + 1) < tableLayoutPanel1.ColumnCount * tableLayoutPanel1.ColumnCount && (tableLayoutPanel1.ColumnCount - 1) - (i % tableLayoutPanel1.ColumnCount) >= 1 && tableLayoutPanel1.Controls[(i + (2 * tableLayoutPanel1.ColumnCount) + 1)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i + (2 * tableLayoutPanel1.ColumnCount) + 1].Enabled = true;
+                tableLayoutPanel1.Controls[i + (2 * tableLayoutPanel1.ColumnCount) + 1].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if ((i + (2 * tableLayoutPanel1.ColumnCount) - 1) < tableLayoutPanel1.ColumnCount * tableLayoutPanel1.ColumnCount && (i % tableLayoutPanel1.ColumnCount) >= 1 && tableLayoutPanel1.Controls[(i + (2 * tableLayoutPanel1.ColumnCount) - 1)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i + (2 * tableLayoutPanel1.ColumnCount) - 1].Enabled = true;
+                tableLayoutPanel1.Controls[i + (2 * tableLayoutPanel1.ColumnCount) - 1].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if (2 <= (i % tableLayoutPanel1.ColumnCount) && 0 <= (i - tableLayoutPanel1.ColumnCount - 2) && tableLayoutPanel1.Controls[(i - tableLayoutPanel1.ColumnCount - 2)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i - tableLayoutPanel1.ColumnCount - 2].Enabled = true;
+                tableLayoutPanel1.Controls[i - tableLayoutPanel1.ColumnCount - 2].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if ((tableLayoutPanel1.ColumnCount - 1) - (i % tableLayoutPanel1.ColumnCount) >= 2 && 0 <= (i - tableLayoutPanel1.ColumnCount + 2) && tableLayoutPanel1.Controls[(i - tableLayoutPanel1.ColumnCount + 2)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i - tableLayoutPanel1.ColumnCount + 2].Enabled = true;
+                tableLayoutPanel1.Controls[i - tableLayoutPanel1.ColumnCount + 2].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if (2 <= (i % tableLayoutPanel1.ColumnCount) && tableLayoutPanel1.ColumnCount * tableLayoutPanel1.ColumnCount > (i + tableLayoutPanel1.ColumnCount - 2) && tableLayoutPanel1.Controls[(i + tableLayoutPanel1.ColumnCount - 2)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i + tableLayoutPanel1.ColumnCount - 2].Enabled = true;
+                tableLayoutPanel1.Controls[i + tableLayoutPanel1.ColumnCount - 2].BackColor = Color.Orange;
+                hamle = true;
+            }
+            if ((tableLayoutPanel1.ColumnCount - 1) - (i % tableLayoutPanel1.ColumnCount) >= 2 && tableLayoutPanel1.ColumnCount * tableLayoutPanel1.ColumnCount > (i + tableLayoutPanel1.ColumnCount + 2) && tableLayoutPanel1.Controls[(i + tableLayoutPanel1.ColumnCount + 2)].Tag.ToString() == 0.ToString())
+            {
+                tableLayoutPanel1.Controls[i + tableLayoutPanel1.ColumnCount + 2].Enabled = true;
+                tableLayoutPanel1.Controls[i + tableLayoutPanel1.ColumnCount + 2].BackColor = Color.Orange;
+                hamle = true;
+            }
+        }
+
+    }
+}
